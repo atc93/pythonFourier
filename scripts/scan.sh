bd579750fcc06e6cda4a65fa1228f73cd886bebd
atc93 <antoine.chapelain@gmail.com>
2018-08-16 11:41:03 -0400

    updates

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -2,11 +2,11 @@ t0=0
 tS=4
 tm=400
 n=0.12
- textfile="txt/frs_9d.txt"
+textfile="txt/frs_9d.txt"
 rootfile="root/frs_9d.root"
 
 rm -f $textfile
-./renice.sh >& renice.log &
+./renice.sh >& log/renice.log &
 
 for itm in {0..0}; do
     tm=$(echo "400+$itm*100" | bc)
@@ -15,7 +15,7 @@ for itm in {0..0}; do
         for it0 in {0..0}; do
             t0=$(echo "-0.31935+$it0*0.0001" | bc)
             echo $t0, $tS, $tm
-            python Data_fourierAnalysis.py $t0 $tS $tm $n $textfile $rootfile -b
+            python python/Data_fourierAnalysis.py $t0 $tS $tm $n $textfile $rootfile -b
         done
     done
 done
